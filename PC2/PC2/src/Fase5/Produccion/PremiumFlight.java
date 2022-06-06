2d7b0fbfd9fddd763da5376aa47c556146afb589
diego <diego.vasquez.l@uni.pe>
2022-06-05 22:31:29 -0500

    question 7 has been resolved (PremiumFlight has been implement)

--- PC2/PC2/src/Fase5/Produccion/PremiumFlight.java
+++ PC2/PC2/src/Fase5/Produccion/PremiumFlight.java
@@ -6,20 +6,43 @@ public class PremiumFlight extends Flight {
     }
 
     /**
-     * @param passenger 
+     * @param passenger
      * @return
      */
     @Override
     public boolean addPassenger(Passenger passenger) {
+
+        /**
+         * Si el pasajero es VIP se podria hacer la operación de añadir al pasajero
+         * usando su metodo add, que devolverá un booleano indicando como se
+         * realizo la operación, el cual a su ves retornaria este metodo,
+         * además, si se ve que el pasajero no es VIP el método devolvería false.
+         */
+
+        if (passenger.isVip()) {
+            return passengers.add(passenger);
+        }
         return false;
     }
 
     /**
-     * @param passenger 
+     * @param passenger
      * @return
      */
     @Override
     public boolean removePassenger(Passenger passenger) {
+
+        /**
+         * De forma similar al anterior método, primero se verifica que el
+         * pasajero a remover sea vip, si es así se procede a remover
+         * y se procede a usar el metodo remove de passenger que
+         * intentará realizar la remoción del pasajero y devolverá un
+         * booleano indicando si se pudo o no realizar, si se ve que no es
+         * VIP, tambien devolverá un FALSE
+         */
+        if (passenger.isVip()) {
+            return passengers.remove(passenger);
+        }
         return false;
     }
 
