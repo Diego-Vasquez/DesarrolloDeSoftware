33524b5a1c1eb72f0bad249e24a3c33429a8bf4b
Abraham ✨ <65505303+AbrahamBerrospiCasano@users.noreply.github.com>
2022-06-05 21:51:53 -0500

    Update AirportTest.java

--- PC2/PC2/src/Fase4/Pruebas/AirportTest.java
+++ PC2/PC2/src/Fase4/Pruebas/AirportTest.java
@@ -117,29 +117,41 @@ public class AirportTest {
 
     @DisplayName("Dado que hay un vuelo premium")
     @Nested
+    //Declaramos la clase PremiumFlightTest
     class PremiumFlightTest {
+        //Creamos objetos de la clase vuelo y pasajeros
         private Flight premiumFlight;
         private Passenger jessica;
         private Passenger cesar;   
         
         @BeforeEach
         void setUp() {
+            //preparamos los objetos para las pruebas
             premiumFlight = new PremiumFlight("3");
             jessica = new Passenger("Jessica", false);
             cesar = new Passenger("Cesar", true);
         }
         
         @Nested
+        //La anotación JUnit @DisplayName se usa para proporcionar un nombre personalizado para la clase de prueba y los métodos de prueba. 
         @DisplayName("Cuando tenemos un pasajero regular")
+        //Declaramos una clase aninada RegularPassenger
         class RegularPassenger {
-
+            
+            //insertamos una prueba
             @Test
+            //especificamos los test con JUnit5 usando Displayname
             @DisplayName("Entonces no puedes agregarlo o quitarlo de un vuelo premium")
             public void testPremiumFlightRegularPassenger() {
+                //usamos el metodo assertAll para verificar multiples condiciones para un vuelo premium y un pasajero regular 
                 assertAll("Verifica todas las condiciones para un pasajero regular y un vuelo premium",
+                        //Comprobamos que no puedemos agregar un pasajero a un vuelo premium. 
                         () -> assertEquals(false, premiumFlight.addPassenger(jessica)),
+                        //Comprobamos que intentar agregar un pasajero no cambia el tamaño de la lista de pasajeros.
                         () -> assertEquals(0, premiumFlight.getPassengersList().size()),
+                        //Comprobamos que no podemos eliminar un pasajero de un vuelo premium.
                         () -> assertEquals(false, premiumFlight.removePassenger(jessica)),
+                        //Comprobamos que intentar eliminar un pasajero no cambia el tamaño de la lista de pasajeros.
                         () -> assertEquals(0, premiumFlight.getPassengersList().size())
                 );
             }
@@ -148,14 +160,20 @@ public class AirportTest {
         @Nested
         @DisplayName("Cuando tenemos un pasajero VIP")
         class VipPassenger {
-
+            
+            //Insertamos otra prueba de un vuelo premium y pasajero vip
             @Test
             @DisplayName("Luego puede agregarlo y eliminarlo de un vuelo premium")
             public void testPremiumFlightVipPassenger() {
+                //usamos otra vez asserAll
                 assertAll("Verifica todas las condiciones para un pasajero VIP y un vuelo premium",
-                        () -> assertEquals(true, premiumFlight.addPassenger(cesar)),
+                        //Compropbamos que podemos agregar un pasajero a un vuelo premium.
+                        () -> assertEquals(true, premiumFlight.addPassenger(cesar
+                        //Comprobamos que al agregar pasajeros aumenta el tamaño de la lista de pasajeros.
                         () -> assertEquals(1, premiumFlight.getPassengersList().size()),
+                        //Comprobamos que podemos eliminar un pasajero de un vuelo premium.
                         () -> assertEquals(true, premiumFlight.removePassenger(cesar)),
+                        //Comprobamos que al eliminar pasajeros disminuye el tamao de la lista de pasajeros.
                         () -> assertEquals(0, premiumFlight.getPassengersList().size())
                 );
             }
